4bdd4934fc6d98603054e95bd6dfba1bc8732d3e
workaround <1584550+workaround@users.noreply.github.com>
2021-07-07 01:15:22 +0200

    Fix TLS issues

--- rss-synd.tcl
+++ rss-synd.tcl
@@ -74,7 +74,7 @@ proc ::rss-synd::init {args} {
 					continue
 				}
 
-				::http::register https 443 ::tls::socket
+				::http::register https 443 [list ::tls::socket -autoservername true]
 			}
 
 			if {(![info exists tmp(url-auth)]) || ($tmp(url-auth) == "")} {
